43fea934d0ee73dcf9069f2cf2e85c6e71db2bcf
haulamle <haulamle@gmail.com>
2022-09-20 00:25:32 +0700

    product

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,5 +1,113 @@
-function Product() {
-    return <h1>Product</h1>;
+import classNames from 'classnames/bind';
+import styles from './Product.module.scss';
+import { Link, useParams } from 'react-router-dom';
+import Button from '~/components/Button';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Product from '~/components/Product';
+import LoadingIcon from '~/components/Loading';
+
+const cx = classNames.bind(styles);
+
+function ProductPage() {
+    const { id } = useParams();
+    const [dataProduct, setDataProduct] = useState([]);
+    const [dataProductFilter, setDataProductFilter] = useState([]);
+    const [totalProduct, setTotalProduct] = useState();
+    const [loading, setLoading] = useState(true);
+    console.log(loading);
+
+    useEffect(() => {
+        setTimeout(() => {
+            axios
+                .get(`http://localhost:5000/product/catogory/${id}`, {
+                    params: {},
+                })
+                .then(function (response) {
+                    setDataProduct(response.data.data);
+                    setDataProductFilter(response.data.data);
+                    setTotalProduct(response.data.total);
+                    setLoading(false);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }, 2000);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    const handleFilter = (e) => {
+        const data = dataProductFilter.filter((product) => product?.name?.toLowerCase()?.includes(e.toLowerCase()));
+        setDataProduct(data);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1400);
+    };
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('img-panner')}>
+                <img src="https://bizweb.dktcdn.net/100/438/408/files/polopc.jpg?v=1657939410701" alt="" />
+            </div>
+            <div className={cx('action-btn')}>
+                <Link to="/">Trang chủ </Link> / <span>BỘ SƯU TẬP</span>
+            </div>
+            <h1>SẢN PHẨM CỦA YODY</h1>
+            <div className={cx('wrap-btn')}>
+                <Button
+                    primary
+                    onClick={() => {
+                        handleFilter('NAM');
+                        setLoading(!loading);
+                    }}
+                >
+                    NAM
+                </Button>
+                <Button
+                    primary
+                    onClick={() => {
+                        handleFilter('NỮ');
+                        setLoading(!loading);
+                    }}
+                >
+                    NỮ
+                </Button>
+                <Button
+                    primary
+                    onClick={() => {
+                        handleFilter('TRẺ EM');
+                        setLoading(!loading);
+                    }}
+                >
+                    TRẺ EM
+                </Button>
+            </div>
+            <div className={cx('wrap-container')}>
+                <div className={cx('selective')}>
+                    <span>Loại Sản Phẩm</span>
+                    <span>Màu sắc</span>
+                    <span>Kích Thước</span>
+                    <span>Chất Liệu</span>
+                    <span>Khoảng Giá</span>
+                </div>
+                <div className={cx('wrap')}>
+                    <div className={cx('wrap-title')}>
+                        <span className={cx('total-product')}>{totalProduct} sản Phẩm</span>
+                        <div className={cx('wrapper-sort')}>
+                            Sắp Xếp Theo <span className={cx('sort')}>Mặc định</span>
+                        </div>
+                    </div>
+                    {loading ? (
+                        <LoadingIcon />
+                    ) : (
+                        <div className={cx('wrapper-product')}>
+                            {dataProduct.map((product) => (
+                                <Product key={product.idSP} data={product} />
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Product;
+export default ProductPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,15 @@
 // import { AdminLayout } from '~/components/Layout';
 
 import Home from '~/pages/Home';
-import Product from '~/pages/Product';
 import Login from '~/pages/Login';
 import Search from '~/pages/Search';
+import ProductPage from '~/pages/Product';
+import ProductDetail from '~/pages/ProductDetail';
 
 const publicRoutes = [
     { path: '/', component: Home },
-    { path: '/product/:id', component: Product },
-    { path: '/product-detail/:id', component: Product },
+    { path: '/product/:id', component: ProductPage },
+    { path: '/product-detail/:id', component: ProductDetail },
     { path: '/search/:type', component: Search },
     { path: '/login', component: Login, layout: null },
     // { path: '/login', component: Login, layout: AdminLayout },
